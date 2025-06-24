bda70e244f8846b5c213f8cbb4c7741462f36591
Flo Rian <florian.kattenbeck@outlook.de>
2025-06-24 20:25:17 +0200

    added backend apiURL in api.js for choreo config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,22 +1,22 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
-const api = axios.create(
-    {
-        baseURL: import.meta.env.VITE_API_URL
-    }
-)
+const apiURL = "/choreo-apis/django-react-fullstackapp/backend-qj/v1";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
-export default api
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default api;
